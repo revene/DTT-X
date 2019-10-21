7b6d6c2693fca2717f2f90a9b7c0849d1023edf7
王保良 <1479930060@qq.com>
2019-10-21 19:03:22 +0800

    恢复

---
+++ src/main/java/com/blanc/dttxweb/DttxWebApplication.java
@@ -0,0 +1,13 @@
+package com.blanc.dttxweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DttxWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DttxWebApplication.class, args);
+	}
+
+}
